1e299ca8055bb591b6690a1695da74ab5a988479
overlayfox <simonkoester91@gmail.com>
2021-08-26 11:44:30 +0200

    finished the file compressor

--- Compressor.py
+++ Compressor.py
@@ -1,10 +1,13 @@
 import lzma
 import py7zr
+import os
+import shutil
 
-my_filter = [{"id": py7zr.FILTER_LZMA2, "preset": 9 | lzma.PRESET_EXTREME}]
+my_filter = [{"id": py7zr.FILTER_LZMA2, "preset": 7}]
 
 with py7zr.SevenZipFile('target.7z', 'w', filters=my_filter) as archive:
-    archive.writeall("/home/overlayfox/Documents/Test")
+    archive.writeall('/home/overlayfox/Documents/Test', 'base')
+    shutil.move("target.7z", "/home/overlayfox/Documents")
 
 
 
