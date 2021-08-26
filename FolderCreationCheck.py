99a305aae35bb01e2ef8789215fbd24b823ec648
overlayfox <simonkoester91@gmail.com>
2021-08-26 07:53:16 +0200

    Cleaned up the folder creation scheme and implimented a counting system for stats

--- FolderCreationCheck.py
+++ FolderCreationCheck.py
@@ -4,25 +4,41 @@ import os
 
 notifier = inotify.adapters.Inotify()
 notifier.add_watch("/home/overlayfox/Documents/Test")
-global foldername
+
+global folder_name
+count_folder = 0
+count_deleted = 0
+count_allowed = 0
+path = "/home/overlayfox/Documents/Test/"
 
 
 def check():
-    matched = re.match("[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9] - ", foldername)
+    global count_deleted
+    global path
+    global count_allowed
+
+    matched = re.match("[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9] - ", folder_name)
     is_match = bool(matched)
     if matched:
-        print("Fine")
+        os.makedirs(path + folder_name + "/00 Export")
+        os.makedirs(path + folder_name + "/01 Footage")
+        os.makedirs(path + folder_name + "/02 Fonts")
+        os.makedirs(path + folder_name + "/03 Graphics")
+        os.makedirs(path + folder_name + "/04 Premiere")
+        os.makedirs(path + folder_name + "/05 AfterEffects")
+        os.makedirs(path + folder_name + "/06 Photoshop")
+        count_allowed = count_allowed + 1
+        print(count_allowed, " folders created")
     else:
-        path = "/home/overlayfox/Documents/Test/"+foldername
-        os.rmdir(path)
-        print("Folder delted")
-
+        os.rmdir(path+folder_name)
+        count_deleted = count_deleted + 1
+        print(count_deleted, " folders deleted")
 
 
 for event in notifier.event_gen():
     if event is not None:
         if "IN_CREATE" in event[1]:
             print("file '{0}' created in '{1}'".format(event[3], event[2]))
-            foldername = event[3]
-            print(foldername)
+            folder_name = event[3]
+            count_folder = count_folder + 1
             check()
